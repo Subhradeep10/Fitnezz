9727d9d0126ce458182c593565253ab72b421b6c
Mohit Namdev <namdevmohit0@gmail.com>
2022-08-06 00:48:34 +0530

    added javascript

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ const navBar = document.getElementsByClassName("navbar")[0];
 toggleBtn.addEventListener("click", ()=> {
     navBar.classList.toggle('active');
 })
+
+
+document.querySelectorAll('.image-container img').forEach(image=>{
+    image.onclick = () =>{
+        document.querySelector('.popup-img').style.display = 'block';
+        document.querySelector('.popup-img img').src = image.getAttribute('src');
+    }
+});
+
+document.querySelector('.popup-img span').onclick = () =>{
+    document.querySelector('.popup-img').style.display = 'none';
+}
