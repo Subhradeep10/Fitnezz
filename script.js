cfbcce806622804b484d16188338f505f30690fd
Vanshita Mathur <vanshitamathur2307@gmail.com>
2022-08-06 13:38:00 +0530

    Added a Testimonial Section
    Added a Testimonial Section with a slider

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ const navBar = document.getElementsByClassName("navbar")[0];
 toggleBtn.addEventListener("click", ()=> {
     navBar.classList.toggle('active');
 })
+const wrapper = document.querySelector('.wrapper');
+const indicators = [...document.querySelectorAll('.indicators button')];
+let currentTestimonial = 0;
+indicators.forEach((item,i) => {
+item.addEventListener('click',() => {
+    indicators[currentTestimonial].classList.remove('active');
+    wrapper.style.marginLeft=`-${100 * i}%`;
+    item.classList.add('active');
+    currentTestimonial = i;
+})
+})
