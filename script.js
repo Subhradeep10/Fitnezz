2bb96b9157d64f5600d2828c8b47a652c1e49c2d
Rajat <rajatsingh52220@gmail.com>
2022-08-06 16:04:55 +0530

    Faq section created

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 const toggleBtn = document.getElementsByClassName("toggle-button")[0];
 const navBar = document.getElementsByClassName("navbar")[0];
 
-toggleBtn.addEventListener("click", ()=> {
-    navBar.classList.toggle('active');
-})
+toggleBtn.addEventListener("click", () => {
+  navBar.classList.toggle("active");
+});
+
+$(function () {
+  $(".accordion")
+    .find(".accordion__title")
+    .click(function () {
+      $(this).toggleClass("active");
+      $(this).next().slideToggle("fast");
+      $(".accordion__content").not($(this).next()).slideUp("fast");
+      $(".accordion__title").not($(this)).removeClass("active");
+    });
+});
